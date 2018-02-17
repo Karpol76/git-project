8cddcf242d59642b52c100de57cefb039e4bdb72
karpol <garankin_a_v@mail.ru>
2018-02-17 17:12:39 +0300

    end

--- Base/MainLogic/Patching/PatchDual.js
+++
@@ -1,7 +0,0 @@
-var dualTable = ObasTableCollection.DualTable;
-if (dualTable.RowCount === 0) {
-    dualTable.IsReadOnly = false;
-    dualTable.AddRow();
-    dualTable.PostRow();
-    dualTable.IsReadOnly = true;
-}
--- Base/UpdateFiles/CreateUpdate.bat
+++
@@ -1,7 +0,0 @@
-cd "UpdateFiles\"
-del Update.upd
-cd ..
-call arch_empty.bat
-"..\..\..\..\EditorPro\Build\Utils\pkzip.exe" -add -rec -path=relative  outlay_empty.zip UpdateFiles\settings.xml
-copy outlay_empty.zip UpdateFiles\Update.upd
-del outlay_empty.zip
